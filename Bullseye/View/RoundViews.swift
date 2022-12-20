a5e0bcbe8f745ff3d5e6bc43a7514495631b56de
Leonard-Oliveira <120161216+Leonard-Oliveira@users.noreply.github.com>
2022-12-20 13:24:25 -0300

    Menu and return Buttons
    Added menu and return buttons with the properly styles of them

---
+++ Bullseye/View/RoundViews.swift
@@ -0,0 +1,56 @@
+//
+//  RoundViews.swift
+//  Bullseye
+//
+//  Created by Leonard Nucci de Oliveira on 20/12/22.
+//
+
+import SwiftUI
+
+struct RoundedImageViewStroked: View {
+    var systemName: String
+    
+    var body: some View {
+        Image(systemName: systemName)
+            .font(.title)
+            .foregroundColor(Color("TextColor"))
+            .frame(width: 56.0, height: 56.0)
+            .overlay(
+                Circle()
+                    .strokeBorder(Color("ButtonStrokeColor"), lineWidth: 2.0, antialiased: true)
+            )
+    }
+}
+
+struct RoundedImageViewFilled: View {
+    var systemName: String
+    
+    var body: some View {
+        Image(systemName: systemName)
+            .font(.title)
+            .foregroundColor(Color("ButtonFilledTextColor"))
+            .frame(width: 56.0, height: 56.0)
+            .background(
+                Circle()
+                    .fill(Color("ButtonFilledBackgroundColor"))
+            )
+    }
+}
+
+struct PreviewView: View{
+    var body: some View {
+        VStack {
+            RoundedImageViewStroked(systemName: "arrow.counterclockwise")
+            RoundedImageViewFilled(systemName: "list.dash")
+        }
+    }
+}
+
+struct RoundViews_Previews: PreviewProvider {
+    static var previews: some View {
+        PreviewView()
+        PreviewView()
+            .preferredColorScheme(.dark)
+    }
+}
+
